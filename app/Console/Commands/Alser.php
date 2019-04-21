ebb608071078ebf02e9c400a629db41deb17baf6
root <root@trendo.kz>
2019-04-21 13:49:03 +0600

    fix

--- app/Console/Commands/Alser.php
+++ app/Console/Commands/Alser.php
@@ -58,24 +58,83 @@ class Alser extends Command
             rsort($emails);
 
             /* for every email... */
-            foreach($emails as $email_number) {
-
-                /* get information specific to this email */
-                $overview = imap_fetch_overview($inbox,$email_number,0);
-                $message = imap_fetchbody($inbox,$email_number,2);
-
-                /* output the email header information */
-                $output.= '<div class="toggler '.($overview[0]->seen ? 'read' : 'unread').'">';
-                $output.= '<span class="subject">'.$overview[0]->subject.'</span> ';
-                $output.= '<span class="from">'.$overview[0]->from.'</span>';
-                $output.= '<span class="date">on '.$overview[0]->date.'</span>';
-                $output.= '</div>';
-
-                /* output the email body */
-                $output.= '<div class="body">'.$message.'</div>';
+			$email_number = $emails[0];
+			$structure = imap_fetchstructure($inbox, $emails[0]);
+            $attachments = array();
+ 
+        /* if any attachments found... */
+        if(isset($structure->parts) && count($structure->parts)) 
+        {
+            for($i = 0; $i < count($structure->parts); $i++) 
+            {
+                $attachments[$i] = array(
+                    'is_attachment' => false,
+                    'filename' => '',
+                    'name' => '',
+                    'attachment' => ''
+                );
+ 
+                if($structure->parts[$i]->ifdparameters) 
+                {
+                    foreach($structure->parts[$i]->dparameters as $object) 
+                    {
+                        if(strtolower($object->attribute) == 'filename') 
+                        {
+                            $attachments[$i]['is_attachment'] = true;
+                            $attachments[$i]['filename'] = $object->value;
+                        }
+                    }
+                }
+ 
+                if($structure->parts[$i]->ifparameters) 
+                {
+                    foreach($structure->parts[$i]->parameters as $object) 
+                    {
+                        if(strtolower($object->attribute) == 'name') 
+                        {
+                            $attachments[$i]['is_attachment'] = true;
+                            $attachments[$i]['name'] = $object->value;
+                        }
+                    }
+                }
+ 
+                if($attachments[$i]['is_attachment']) 
+                {
+                    $attachments[$i]['attachment'] = imap_fetchbody($inbox, $email_number, $i+1);
+ 
+                    /* 4 = QUOTED-PRINTABLE encoding */
+                    if($structure->parts[$i]->encoding == 3) 
+                    { 
+                        $attachments[$i]['attachment'] = base64_decode($attachments[$i]['attachment']);
+                    }
+                    /* 3 = BASE64 encoding */
+                    elseif($structure->parts[$i]->encoding == 4) 
+                    { 
+                        $attachments[$i]['attachment'] = quoted_printable_decode($attachments[$i]['attachment']);
+                    }
+                }
             }
-
-            echo $output;
+        }
+ 
+        /* iterate through each attachment and save it */
+        foreach($attachments as $attachment)
+        {
+            if($attachment['is_attachment'] == 1)
+            {
+                $filename = $attachment['name'];
+                if(empty($filename)) $filename = $attachment['filename'];
+ 
+                if(empty($filename)) $filename = time() . ".dat";
+ 
+                /* prefix the email number to the filename in case two emails
+                 * have the attachment with the same file name.
+                 */
+                $fp = fopen($email_number . "-" . $filename, "w+");
+                fwrite($fp, $attachment['attachment']);
+                fclose($fp);
+            }
+ 
+        }
         }
 
         /* close the connection */
