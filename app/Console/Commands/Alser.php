1bcf38ecbec6af83cd6c69a56690690d78ac2374
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-04-21 13:22:15 +0600

    fix

--- app/Console/Commands/Alser.php
+++ app/Console/Commands/Alser.php
@@ -38,7 +38,7 @@ class Alser extends Command
     public function handle()
     {
         /* connect to gmail */
-        $hostname = "{imap.gmail.com:993/imap/ssl}INBOX";
+        $hostname = "{imap.gmail.com:993/imap/ssl/novalidate-cert/norsh}INBOX";
         $username = 'optpricealmaty@gmail.com';
         $password = 'Optprice2019@#';
 
