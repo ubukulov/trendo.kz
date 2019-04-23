12714d41d376a58c46b55394c2feef9a9bce4390
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-04-23 13:26:16 +0600

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('alser:update')->everyThirtyMinutes();
+        $schedule->command('alser:update')->everyFiveMinutes();
     }
 
     /**
