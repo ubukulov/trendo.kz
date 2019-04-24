775c50089ff7bcdb0cdb87e79d0949d99e48f7d2
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-04-24 14:40:05 +0600

    fix

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -22,11 +22,11 @@
                                 <span class="onsale">Sale!</span>
                                 <div class="images electro-gallery">
                                     <div class="thumbnails-single owl-carousel">
-                                        <a href="/assets/images/single-product/s1-1.jpg" class="zoom" title="" data-rel="prettyPhoto[product-gallery]"><img src="{{ $product->images }}" data-echo="{{ $product->images }}" class="wp-post-image" alt=""></a>
+                                        <a href="{{ $product->images }}" class="zoom" title="" data-rel="prettyPhoto[product-gallery]"><img src="{{ $product->images }}" data-echo="{{ $product->images }}" class="wp-post-image" alt=""></a>
                                     </div><!-- .thumbnails-single -->
 
                                     <div class="thumbnails-all columns-5 owl-carousel">
-                                        <a href="/assets/images/single-product/single-thumb1.jpg" class="first" title=""><img src="/assets/images/blank.gif" data-echo="/assets/images/single-product/single-thumb1.jpg" class="wp-post-image" alt=""></a>
+                                        <a href="{{ $product->images }}" class="first" title=""><img src="{{ $product->images }}" data-echo="{{ $product->images }}" class="wp-post-image" alt=""></a>
                                     </div><!-- .thumbnails-all -->
                                 </div><!-- .electro-gallery -->
                             </div><!-- /.product-images-wrapper -->
@@ -321,44 +321,7 @@
 
                                 <div class="tab-pane active in panel entry-content wc-tab" id="tab-description">
                                     <div class="electro-description">
-
-                                        <h3>Perfectly Done</h3>
-                                        <p>Praesent ornare, ex a interdum consectetur, lectus diam sodales elit, vitae egestas est enim ornare nisl. Nullam in lectus nec sem semper viverra. In lobortis egestas massa. Nam nec massa nisi. Suspendisse potenti. Quisque suscipit vulputate dui quis volutpat. Ut id elit facilisis, feugiat est in, tempus lacus. Ut ultrices dictum metus, a ultricies ex vulputate ac. Ut id cursus tellus, non tempor quam. Morbi porta diam nisi, id finibus nunc tincidunt eu.</p>
-                                        <table class="layout">
-                                            <tbody>
-                                            <tr>
-                                                <td>
-                                                    <h3>Wireless</h3>
-                                                    <p>Fusce vitae nibh mi. Integer posuere, libero et ullamcorper facilisis, enim eros tincidunt orci, eget vestibulum sapien nisi ut leo. Cras finibus vel est ut mollis. Donec luctus condimentum ante et euismod.</p>
-                                                    <h3>Fresh Design</h3>
-                                                    <p>Integer bibendum aliquet ipsum, in ultrices enim sodales sed. Quisque ut urna vitae lacus laoreet malesuada eu at massa. Pellentesque nibh augue, pellentesque nec dictum vel, pretium a arcu. Duis eu urna suscipit, lobortis elit quis, ullamcorper massa.</p>
-                                                    <h3>Fabolous Sound</h3>
-                                                    <p>Cras rutrum, nibh a sodales accumsan, elit sapien ultrices sapien, eget semper lectus ex congue elit. Nullam dui elit, fermentum a varius at, iaculis non dolor. In hac habitasse platea dictumst.
-                                                </td>
-
-                                                <td>
-                                                    <img class="alignright" data-echo="assets//assets/images/single-product/ForDescription.jpg" src="/assets/images/blank.gif" alt="">
-                                                </td>
-                                            </tr>
-                                            </tbody>
-                                        </table>
-
-                                        <table class="layout">
-                                            <tbody>
-                                            <tr>
-                                                <td>
-                                                    <img class="alignnone" data-echo="assets//assets/images/single-product/ForDescription-1.png" src="/assets/images/blank.gif" alt="">
-                                                </td>
-
-                                                <td>
-                                                    <h3 style="text-align: right;">Inteligent Bass</h3>
-                                                    <p style="text-align: right;">Fusce vitae nibh mi. Integer posuere, libero et ullamcorper facilisis, enim eros tincidunt orci, eget vestibulum sapien nisi ut leo. Cras finibus vel est ut mollis. Donec luctus condimentum ante et euismod.</p>
-                                                    <h3 style="text-align: right;">Battery Life</h3>
-                                                    <p style="text-align: right;">Integer bibendum aliquet ipsum, in ultrices enim sodales sed. Quisque ut urna vitae lacus laoreet malesuada eu at massa. Pellentesque nibh augue, pellentesque nec dictum vel, pretium a arcu. Duis eu urna suscipit, lobortis elit quis, ullamcorper massa.</p>
-                                                </td>
-                                            </tr>
-                                            </tbody>
-                                        </table>
+                                        {{ $product->full_description }}
                                     </div><!-- /.electro-description -->
 
                                     <div class="product_meta">
