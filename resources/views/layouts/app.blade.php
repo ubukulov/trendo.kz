b7056ae6a8b4caf97c6403280b9c5414b004aa84
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-06-06 15:02:25 +0600

    js fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,16 +36,6 @@
 
 </div><!-- #page -->
 
-<script type="text/javascript" src="{{ asset('assets/js/jquery.min.js') }}"></script>
-<script type="text/javascript" src="/assets/js/tether.min.js"></script>
-<script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="/assets/js/bootstrap-hover-dropdown.min.js"></script>
-<script type="text/javascript" src="/assets/js/owl.carousel.min.js"></script>
-<script type="text/javascript" src="/assets/js/echo.min.js"></script>
-<script type="text/javascript" src="/assets/js/wow.min.js"></script>
-<script type="text/javascript" src="/assets/js/jquery.easing.min.js"></script>
-<script type="text/javascript" src="/assets/js/jquery.waypoints.min.js"></script>
-<script type="text/javascript" src="/assets/js/electro.js"></script>
-<script type="text/javascript" src="/js/checkout.js"></script>
+@include('partials.js')
 </body>
 </html>
