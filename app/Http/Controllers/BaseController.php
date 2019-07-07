e401ffa48e96e18339bd905e691d76389783a46c
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-07-07 11:47:10 +0600

    fix

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -16,8 +16,14 @@ class BaseController extends Controller
     {
         $this->cats = Category::get()->toTree();
         $this->subCategories = Category::withDepth()->having('depth', '=', 2)->get();
+        $recommended_products = Product::whereNotNull('images')->inRandomOrder()->take(6)->get();
+        $on_sales = Product::whereNotNull('images')->inRandomOrder()->take(6)->get();
+        $most_populars = Product::whereNotNull('images')->inRandomOrder()->take(6)->get();
         View::share('cats', $this->cats);
         View::share('subCategories', $this->subCategories);
         View::share('page', $this->page);
+        View::share('recommended_products', $recommended_products);
+        View::share('on_sales', $on_sales);
+        View::share('most_populars', $most_populars);
     }
 }
