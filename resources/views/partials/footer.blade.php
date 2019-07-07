b21c4ae32dee71c9d475a1c9b9065e970906b9ed
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-07-07 10:59:12 +0600

    fix

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,28 +7,15 @@
                         <div class="body">
                             <h4 class="widget-title">@lang('messages.Featured Products')</h4>
                             <ul class="product_list_widget">
+                                @foreach($on_sales as $sale)
                                 <li>
-                                    <a href="single-product.html" title="Tablet Thin EliteBook  Revolve 810 G6">
-                                        <img class="wp-post-image" data-echo="/assets/images/footer/1.jpg" src="/assets/images/blank.gif" alt="">
-                                        <span class="product-title">Tablet Thin EliteBook  Revolve 810 G6</span>
+                                    <a href="{{ $sale->url() }}" title="{{ $sale->title }}">
+                                        <img class="wp-post-image" data-echo="{{ $sale->getImage() }}" src="{{ $sale->getImage() }}" alt="">
+                                        <span class="product-title">{{ $sale->getImage() }}</span>
                                     </a>
-                                    <span class="electro-price"><span class="amount">&#36;1,300.00</span></span>
+                                    <span class="electro-price"><span class="amount">{!! format_price($sale->getPrice()) !!} &#8376;</span></span>
                                 </li>
-
-                                <li>
-                                    <a href="single-product.html" title="Smartphone 6S 128GB LTE">
-                                        <img class="wp-post-image" data-echo="/assets/images/footer/2.jpg" src="/assets/images/blank.gif" alt=""><span class="product-title">Smartphone 6S 128GB LTE</span>
-                                    </a>
-                                    <span class="electro-price"><span class="amount">&#36;780.00</span></span>
-                                </li>
-
-                                <li>
-                                    <a href="single-product.html" title="Smartphone 6S 64GB LTE">
-                                        <img class="wp-post-image" data-echo="/assets/images/footer/3.jpg" src="/assets/images/blank.gif" alt="">
-                                        <span class="product-title">Smartphone 6S 64GB LTE</span>
-                                    </a>
-                                    <span class="electro-price"><span class="amount">&#36;1,215.00</span></span>
-                                </li>
+                                @endforeach
                             </ul>
                         </div>
                     </aside>
@@ -37,29 +24,15 @@
                     <aside class="widget clearfix">
                         <div class="body"><h4 class="widget-title">@lang('messages.Onsale Products')</h4>
                             <ul class="product_list_widget">
-                                <li>
-                                    <a href="single-product.html" title="Notebook Black Spire V Nitro  VN7-591G">
-                                        <img class="wp-post-image" data-echo="/assets/images/footer/3.jpg" src="/assets/images/blank.gif" alt="">
-                                        <span class="product-title">Notebook Black Spire V Nitro  VN7-591G</span>
-                                    </a>
-                                    <span class="electro-price"><ins><span class="amount">&#36;1,999.00</span></ins> <del><span class="amount">&#36;2,299.00</span></del></span>
-                                </li>
-
-                                <li>
-                                    <a href="single-product.html" title="Tablet Red EliteBook  Revolve 810 G2">
-                                        <img class="wp-post-image" data-echo="/assets/images/footer/4.jpg" src="/assets/images/blank.gif" alt="">
-                                        <span class="product-title">Tablet Red EliteBook  Revolve 810 G2</span>
-                                    </a>
-                                    <span class="electro-price"><ins><span class="amount">&#36;1,999.00</span></ins> <del><span class="amount">&#36;2,299.00</span></del></span>
-                                </li>
-
-                                <li>
-                                    <a href="single-product.html" title="Widescreen 4K SUHD TV">
-                                        <img class="wp-post-image" data-echo="/assets/images/footer/5.jpg" src="/assets/images/blank.gif" alt="">
-                                        <span class="product-title">Widescreen 4K SUHD TV</span>
-                                    </a>
-                                    <span class="electro-price"><ins><span class="amount">&#36;2,999.00</span></ins> <del><span class="amount">&#36;3,299.00</span></del></span>
-                                </li>
+                                @foreach($recommended_products as $r)
+                                    <li>
+                                        <a href="{{ $r->url() }}" title="{{ $r->title }}">
+                                            <img class="wp-post-image" data-echo="{{ $r->getImage() }}" src="{{ $r->getImage() }}" alt="">
+                                            <span class="product-title">{{ $r->getImage() }}</span>
+                                        </a>
+                                        <span class="electro-price"><span class="amount">{!! format_price($r->getPrice()) !!} &#8376;</span></span>
+                                    </li>
+                                @endforeach
                             </ul>
                         </div>
                     </aside>
@@ -69,29 +42,15 @@
                         <div class="body">
                             <h4 class="widget-title">@lang('messages.Top Rated Products')</h4>
                             <ul class="product_list_widget">
-                                <li>
-                                    <a href="single-product.html" title="Notebook Black Spire V Nitro  VN7-591G">
-                                        <img class="wp-post-image" data-echo="/assets/images/footer/6.jpg" src="/assets/images/blank.gif" alt="">
-                                        <span class="product-title">Notebook Black Spire V Nitro  VN7-591G</span>
-                                    </a>
-                                    <div class="star-rating" title="Rated 5 out of 5"><span style="width:100%"><strong class="rating">5</strong> out of 5</span></div>		<span class="electro-price"><ins><span class="amount">&#36;1,999.00</span></ins> <del><span class="amount">&#36;2,299.00</span></del></span>
-                                </li>
-
-                                <li>
-                                    <a href="single-product.html" title="Apple MacBook Pro MF841HN/A 13-inch Laptop">
-                                        <img class="wp-post-image" data-echo="/assets/images/footer/7.jpg" src="/assets/images/blank.gif" alt="">
-                                        <span class="product-title">Apple MacBook Pro MF841HN/A 13-inch Laptop</span>
-                                    </a>
-                                    <div class="star-rating" title="Rated 5 out of 5"><span style="width:100%"><strong class="rating">5</strong> out of 5</span></div>		<span class="electro-price"><span class="amount">&#36;1,800.00</span></span>
-                                </li>
-
-                                <li>
-                                    <a href="single-product.html" title="Tablet White EliteBook Revolve  810 G2">
-                                        <img class="wp-post-image" data-echo="/assets/images/footer/2.jpg" src="/assets/images/blank.gif" alt="">
-                                        <span class="product-title">Tablet White EliteBook Revolve  810 G2</span>
-                                    </a>
-                                    <div class="star-rating" title="Rated 5 out of 5"><span style="width:100%"><strong class="rating">5</strong> out of 5</span></div>		<span class="electro-price"><span class="amount">&#36;1,999.00</span></span>
-                                </li>
+                                @foreach($most_populars as $m)
+                                    <li>
+                                        <a href="{{ $m->url() }}" title="{{ $m->title }}">
+                                            <img class="wp-post-image" data-echo="{{ $m->getImage() }}" src="{{ $m->getImage() }}" alt="">
+                                            <span class="product-title">{{ $m->getImage() }}</span>
+                                        </a>
+                                        <span class="electro-price"><span class="amount">{!! format_price($m->getPrice()) !!} &#8376;</span></span>
+                                    </li>
+                                @endforeach
                             </ul>
                         </div>
                     </aside>
