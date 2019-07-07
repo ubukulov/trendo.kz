edc41504d5afe6508251a486fc611f5a3902e072
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-07-07 10:40:43 +0600

    fix

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -13,7 +13,7 @@ class IndexController extends BaseController
         $recommended_products = Product::whereNotNull('images')->inRandomOrder()->take(6)->get();
         $on_sales = Product::whereNotNull('images')->inRandomOrder()->take(6)->get();
         $most_populars = Product::whereNotNull('images')->inRandomOrder()->take(6)->get();
-        $special_offer = Product::whereNotNull('images')->inRandomOrder()->take(1)->get();
+        $special_offer = Product::whereNotNull('images')->inRandomOrder()->first();
         return view('welcome', compact('recommended_products', 'on_sales', 'most_populars', 'special_offer'));
     }
 }
